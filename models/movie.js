46960a749f799ea3742223603fcd79a09a736aa9
Cardiel Butler <cardielbutler@gmail.com>
2019-04-24 09:25:15 -0400

    updating character count for strings pt.1

--- models/movie.js
+++ models/movie.js
@@ -21,7 +21,7 @@ module.exports = function(sequelize, DataTypes){
       allowNull: false
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(1000),
       allowNull: false
     },
   })
